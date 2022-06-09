0c5b87e9316a1841c9636f16808dd5cd3f2a2bac
kirby00001 <t3273139612@hotmail.com>
2022-06-09 22:44:11 +0800

    最粗层的对齐

--- alignment/alignment.py
+++ alignment/alignment.py
@@ -1,7 +1,9 @@
+from re import A
+from turtle import up
 import cv2
 import numpy as np
 from cv2 import pyrDown
-from .utils import get_tiles, gaussian_pyramid
+from .utils import get_tiles, gaussian_pyramid, match_templates, select_offsets
 
 
 def select_reference_frame(burstPath, imageList, options):
@@ -14,8 +16,12 @@ def select_reference_frame(burstPath, imageList, options):
     return
 
 
-def gaussian_alignment(ref_image, alt_images, tile_size, search_radius_list=[4, 4, 2, 1], sampling_ratios=[1, 2, 4, 4],
-                       norm_list=["L2", "L2", "L2", "L1"]):
+def gaussian_align(ref_image,
+                   alt_images,
+                   tile_size,
+                   search_radius_list=[4, 4, 2, 1],
+                   sampling_ratios=[1, 2, 4, 4],
+                   norm_list=["L2", "L2", "L2", "L1"]):
     """
     四层从粗到细的高斯金字塔对齐
 
@@ -30,29 +36,35 @@ def gaussian_alignment(ref_image, alt_images, tile_size, search_radius_list=[4, 
     up_sample_ratios = [1] + sampling_ratios[:0:-1]
     # 划分参考图像
     ref_tiles = get_tiles(ref_image, tile_size, tile_size // 2)
-    aligned_tiles = np.empty(
-        shape=((len(alt_images),) + ref_tiles.shape), dtype=ref_image.dtype)
+    aligned_tiles = np.empty(shape=((len(alt_images), ) + ref_tiles.shape), dtype=ref_image.dtype)
     aligned_tiles[0] = ref_tiles
-    motion_vectors = np.empty(
-        (len(alt_images), ref_tiles.shape[0], ref_tiles.shape[1], 2))
+    motion_vectors = np.empty((len(alt_images), ref_tiles.shape[0], ref_tiles.shape[1], 2))
     # 构造四层高斯金字塔
     ref_pyr = gaussian_pyramid(ref_image, sampling_ratios)
     # 根据参考图像，对齐每一个候选图像
     for i, alt_image in enumerate(alt_images):
         alt_pyr = gaussian_pyramid(alt_image, sampling_ratios)
         motion_vector = None
-        for ref_level, alt_level, search_radius, norm, up_sample_ratio in zip(ref_pyr, alt_pyr,
-                                                                              search_radius_list,
-                                                                              norm_list,
-                                                                              up_sample_ratios):
-            motion_vector = level_alignment(ref_level, alt_level, tile_size, search_radius, norm,
-                                            pre_align=motion_vector,
-                                            pre_tile_size=tile_size,
-                                            up_sample_ratio=up_sample_ratio)
+        for ref_level, alt_level, search_radius, norm, up_sample_ratio in zip(
+                ref_pyr, alt_pyr, search_radius_list, norm_list, up_sample_ratios):
+            motion_vector = level_align(ref_level,
+                                        alt_level,
+                                        tile_size,
+                                        search_radius,
+                                        norm,
+                                        pre_align=motion_vector,
+                                        pre_tile_size=tile_size,
+                                        up_sample_ratio=up_sample_ratio)
 
 
-def level_alignment(ref_level, alt_level, tile_size, search_radius, norm, pre_align=None, pre_tile_size=None,
-                    up_sample_ratio=None):
+def level_align(ref_level,
+                alt_level,
+                tile_size,
+                search_radius,
+                norm,
+                pre_align=None,
+                pre_tile_size=None,
+                up_sample_ratio=None):
     """
     计算高斯金字塔的一层的运动向量
 
@@ -66,26 +78,39 @@ def level_alignment(ref_level, alt_level, tile_size, search_radius, norm, pre_al
     :param up_sample_ratio: 上采样倍率
     :return: 备用图相对参考图的运动向量
     """
-    ref_tiles = get_tiles(ref_level, tile_size, tile_size // 2)
+    ref_tiles = get_tiles(ref_level, tile_size, step=tile_size // 2)
+    h, w = ref_tiles.shape[0], ref_tiles.shape[1]
     # 如果存在上层对齐计算的运动向量，将其上采样到当前层级
     if pre_align is None:
-        upsampled_pre_align = np.zeros(
-            shape=(ref_tiles.shape[0], ref_tiles.shape[1], 2))
+        upsampled_pre_align = np.zeros(shape=(h, w, 2))
     else:
-        upsampled_pre_align = up_sample_previous_alignment(ref_level, alt_level, pre_align, tile_size, pre_tile_size,
+        upsampled_pre_align = up_sample_previous_alignment(ref_level, alt_level, pre_align,
+                                                           tile_size, pre_tile_size,
                                                            up_sample_ratio)
-    # 计算搜索范围
-
-    # 模板匹配
-    # 获取最小距离对应的块的索引
-    # 计算当前层每块的偏移量
-    offsets = np.zeros(
-        (upsampled_pre_align.shape[0] * upsampled_pre_align.shape[1]), dtype=np.float32)
-
+    # 提取u0, v0
+    u0 = np.round(up_sample_previous_alignment[:, :, :0]).astype(np.int32)
+    v0 = np.round(up_sample_previous_alignment[:, :, :1]).astype(np.int32)
+    # 处理边界问题，并计算搜索范围
+    padded_alt_level = np.pad(alt_level, search_radius, mode="constant", constant_values=2**16 - 1)
+    search_areas = get_tiles(padded_alt_level, tile_size + 2 * search_radius, step=1)
+    # 根据上一层的初始化偏移量，计算搜索范围索引，修正搜索范围
+    i = (np.arange(h).reshape(h, 1).repeat(w, axis=1).reshape(h, w) * tile_size // 2 + u0).reshape(
+        h * w).clip(0, search_areas.shape[0] - 1)
+    j = (np.arange(w).reshape(1, w).repeat(h, axis=0).reshape(h, w) * tile_size // 2 + v0).reshape(
+        h * w).clip(0, search_areas.shape[1] - 1)
+    search_areas = search_areas[i, j].reshape(h, w, search_areas.shape[2], search_areas.shape[3])
+    # 模板匹配,计算距离
+    distances = match_templates(ref_tiles, search_areas, norm)
+    # 获取最小距离对应的块的位置，即相对运动向量
+    offsets = select_offsets(distances)
+    # 上面的相对运动向量是相对(u0,v0)-radius的，修正偏差
+    offsets[:, :, 0] += (u0 - search_radius)
+    offsets[:, :, 1] += (v0 - search_radius)
     return offsets
 
 
-def up_sample_previous_alignment(ref_level, alt_level, pre_align, tile_size, pre_tile_size, up_sample_ratio):
+def up_sample_previous_alignment(ref_level, alt_level, pre_align, tile_size, pre_tile_size,
+                                 up_sample_ratio):
     """
     将上个层级的对齐得到的运动向量上采样到当前层级
 
@@ -101,76 +126,63 @@ def up_sample_previous_alignment(ref_level, alt_level, pre_align, tile_size, pre
     pre_align *= up_sample_ratio
     # 当前层图块的数量/上一层图块的数量
     repeat_ratio = up_sample_ratio // (tile_size // pre_tile_size)
-    up_sampled_align = pre_align.repeat(
-        repeat_ratio, axis=0).repeat(repeat_ratio, axis=1)
+    up_sampled_align = pre_align.repeat(repeat_ratio, axis=0).repeat(repeat_ratio, axis=1)
     h, w = up_sampled_align.shape[0], up_sampled_align.shape[1]
     # 在运动向量边界填充，解决分块计算邻居时的边界问题
-    padded_pre_align = np.pad(
-        pre_align, pad_width=((1,), (1,), (0,)), mode='edge')
+    padded_pre_align = np.pad(pre_align, pad_width=((1, ), (1, ), (0, )), mode='edge')
     # 选择三个最临近的块当作候选块
     tile = np.empty(shape=(repeat_ratio, repeat_ratio, 2, 2), dtype=np.int)
     index = repeat_ratio // 2
     # 上、左
-    tile[:index, :index] = [[-1, 0],
-                            [0, -1]]
+    tile[:index, :index] = [[-1, 0], [0, -1]]
     # 上、右
-    tile[:index, index:] = [[-1, 0],
-                            [0, 1]]
+    tile[:index, index:] = [[-1, 0], [0, 1]]
     # 下、左
-    tile[index:, :index] = [[1, 0],
-                            [0, -1]]
+    tile[index:, :index] = [[1, 0], [0, -1]]
     # 下、右
-    tile[index:, index:] = [[1, 0],
-                            [0, 1]]
+    tile[index:, index:] = [[1, 0], [0, 1]]
     # 从一个图块扩大到整张图的大小
-    neighbors_mask = np.tile(tile, (
-        up_sampled_align.shape[0] // repeat_ratio,
-        up_sampled_align.shape[1] // repeat_ratio,
-        1, 1))
+    neighbors_mask = np.tile(tile, (up_sampled_align.shape[0] // repeat_ratio,
+                                    up_sampled_align.shape[1] // repeat_ratio, 1, 1))
     # 上下邻居的索引
-    vertical_i = np.repeat(
-        np.clip(2 + np.arange(h) // repeat_ratio + neighbors_mask[:, 0, 0, 0], 0,
-                padded_pre_align.shape[0] - 1).reshape(h, 1),
-        w, axis=1).reshape(h * w)
-    vertical_j = np.repeat(
-        np.clip(2 + np.arange(w) // repeat_ratio + neighbors_mask[0, :, 0, 1], 0,
-                padded_pre_align.shape[1] - 1).reshape(1, w),
-        h, axis=0).reshape(h * w)
+    vertical_i = np.repeat(np.clip(2 + np.arange(h) // repeat_ratio + neighbors_mask[:, 0, 0, 0], 0,
+                                   padded_pre_align.shape[0] - 1).reshape(h, 1),
+                           w,
+                           axis=1).reshape(h * w)
+    vertical_j = np.repeat(np.clip(2 + np.arange(w) // repeat_ratio + neighbors_mask[0, :, 0, 1], 0,
+                                   padded_pre_align.shape[1] - 1).reshape(1, w),
+                           h,
+                           axis=0).reshape(h * w)
     # 左右邻居的索引
-    horizontal_i = np.repeat(
-        np.clip(2 + np.arange(h) // repeat_ratio + neighbors_mask[:, 0, 1, 0], 0,
-                padded_pre_align.shape[0] - 1).reshape(h, 1),
-        w, axis=1).reshape(h * w)
-    horizontal_j = np.repeat(
-        np.clip(2 + np.arange(w) // repeat_ratio + neighbors_mask[0, :, 1, 1], 0,
-                padded_pre_align.shape[1] - 1).reshape(1, w),
-        h, axis=0).reshape(h * w)
+    horizontal_i = np.repeat(np.clip(2 + np.arange(h) // repeat_ratio + neighbors_mask[:, 0, 1, 0],
+                                     0, padded_pre_align.shape[0] - 1).reshape(h, 1),
+                             w,
+                             axis=1).reshape(h * w)
+    horizontal_j = np.repeat(np.clip(2 + np.arange(w) // repeat_ratio + neighbors_mask[0, :, 1, 1],
+                                     0, padded_pre_align.shape[1] - 1).reshape(1, w),
+                             h,
+                             axis=0).reshape(h * w)
     # 提取出对应的邻居的运动向量
-    vertical_neighbour = padded_pre_align[vertical_i, vertical_j].reshape(
-        (h, w, 2))
-    horizontal_neighbour = padded_pre_align[horizontal_i, horizontal_j].reshape(
-        (h, w, 2))
+    vertical_neighbour = padded_pre_align[vertical_i, vertical_j].reshape((h, w, 2))
+    horizontal_neighbour = padded_pre_align[horizontal_i, horizontal_j].reshape((h, w, 2))
     # 获取所有可能的参考和候选图块
     ref_tiles = get_tiles(ref_level, tile_size, 1)
     alt_tiles = get_tiles(alt_level, tile_size, 1)
     # TODO 计算参考图块和候选图块的距离
-    d0 = computeTilesDistanceL1_(
-        ref_tiles, alt_tiles, up_sampled_align).reshape(h * w)
-    d1 = computeTilesDistanceL1_(
-        ref_tiles, alt_tiles, vertical_neighbour).reshape(h * w)
-    d2 = computeTilesDistanceL1_(
-        ref_tiles, alt_tiles, horizontal_neighbour).reshape(h * w)
+    d0 = computeTilesDistanceL1_(ref_tiles, alt_tiles, up_sampled_align).reshape(h * w)
+    d1 = computeTilesDistanceL1_(ref_tiles, alt_tiles, vertical_neighbour).reshape(h * w)
+    d2 = computeTilesDistanceL1_(ref_tiles, alt_tiles, horizontal_neighbour).reshape(h * w)
     # TODO 构建候选运动向量
     candidate_align = np.empty((h * w, 3, 2))
     candidate_align[:, 0, :] = up_sampled_align.reshape(h * w, 2)
     candidate_align[:, 1, :] = vertical_neighbour.reshape(h * w, 2)
     candidate_align[:, 2, :] = horizontal_neighbour.reshape(h * w, 2)
     # TODO 选出运动项链，通过最小化L1距离
-    selected_align = candidate_align[np.arange(
-        h * w), np.argmin([d0, d1, d2], axis=0)].reshape((h, w, 2))
+    selected_align = candidate_align[np.arange(h * w),
+                                     np.argmin([d0, d1, d2], axis=0)].reshape((h, w, 2))
     return up_sampled_align
 
 
 if __name__ == "__main__":
     img = np.random.randint(low=0, high=255, size=(3000, 3000), dtype="uint8")
-    print((8,) + img.shape)
+    print((8, ) + img.shape)
--- alignment/utils.py
+++ alignment/utils.py
@@ -1,10 +1,10 @@
-import math
-
+from asyncio.windows_events import NULL
+from re import M
 import cv2
-from cv2 import pyrDown, matchTemplate, minMaxLoc
-
+import math
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
+from cv2 import pyrDown, matchTemplate, minMaxLoc
 
 
 def downSample(fullSize):
@@ -14,7 +14,8 @@ def downSample(fullSize):
     :param fullSize: raw file in ndarray format
     :return: 平均后的灰度图
     """
-    return fullSize[0::2, 0::2] + fullSize[1::2, 0::2] + fullSize[0::2, 1::2] + fullSize[1::2, 1::2] // 4
+    return fullSize[0::2, 0::2] + fullSize[1::2, 0::2] + fullSize[0::2,
+                                                                  1::2] + fullSize[1::2, 1::2] // 4
 
 
 def get_tiles(image, tile_size, step):
@@ -59,6 +60,7 @@ def gaussian_pyramid(image, sampling_ratios=[1, 2, 4, 4]):
 
 def computeTilesDistanceL1_(ref_tiles, alt_tiles, offsets, result):
     """
+    计算采用该偏移量后，参考图块和备选图块之间的L1距离，用作评估偏移量的指标
 
     :param ref_tiles: 参考图块
     :param alt_tiles: 备选图块
@@ -69,28 +71,51 @@ def computeTilesDistanceL1_(ref_tiles, alt_tiles, offsets, result):
     # Dimension
     m, n, tile_size, _ = ref_tiles.shape
     h, w, _ = offsets.shape
-    # Loop over the aligned tiles
+    # 遍历所有对齐的图块
     for i in range(h):
         for j in range(w):
-            # Offset values
-            offI = offsets[i, j, 0]
-            offJ = offsets[i, j, 1]
-            # Reference index
-            ri = i * (tile_size // 2)
-            rj = j * (tile_size // 2)
-            # Deduce the position of the corresponding tiles
-            di = ri + int(offI + (0.5 if offI >= 0 else -0.5))
-            dj = rj + int(offJ + (0.5 if offJ >= 0 else -0.5))
+            # 偏移量
+            i_offset = offsets[i, j, 0]
+            j_offset = offsets[i, j, 1]
+            # 参考图块的索引
+            ref_i = i * (tile_size // 2)
+            ref_j = j * (tile_size // 2)
+            # 对应图块的索引
+            di = ref_i + int(i_offset + (0.5 if i_offset >= 0 else -0.5))
+            dj = ref_j + int(j_offset + (0.5 if j_offset >= 0 else -0.5))
             # Clip the position
             di = 0 if di < 0 else (m - 1 if di > m - 1 else di)
             dj = 0 if dj < 0 else (n - 1 if dj > n - 1 else dj)
-            # Compute the distance
+            # 计算L1距离
             dst = 0
             for p in range(tile_size):
                 for q in range(tile_size):
-                    dst += math.fabs(ref_tiles[ri, rj, p, q] - alt_tiles[di, dj, p, q])
+                    dst += math.fabs(ref_tiles[ref_i, ref_j, p, q] - alt_tiles[di, dj, p, q])
             # Store the result
             result[i, j] = dst
+
+
+def match_templates(ref_tiles, search_areas, mode):
+    if mode == "L2":
+        mode = cv2.TM_SQDIFF
+    h, w, _, _ = ref_tiles.shape
+    distances = np.empty(shape=(h, w, -1, -1), dtype=np.float32)
+    for i in range(h):
+        for j in range(w):
+            distances[h, w, :, :] = matchTemplate(image=search_areas[i, j, :, :],
+                                                  templ=ref_tiles[i, j, :, :],
+                                                  method=mode)
+    return distances
+
+
+def select_offsets(distance):
+    h, w, _, _ = distance.shape
+    offsets = np.empty(shape=[h, w, 2], dtype=np.int32)
+    for i in range(h):
+        for j in range(w):
+            _, _, min_index, _ = minMaxLoc(distance[h, w, :, :])
+            offsets[h, w, :] = np.array(min_index)
+    return offsets
 
 
 if __name__ == "__main__":
